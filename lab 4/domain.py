72b5a1b2b514040d4b6386db94f30fae645db9d8
Serghei Covtun <w.serghei.covtun@gmail.com>
2022-11-24 09:32:20 +0200

    4 lab

---
+++ lab 4/domain.py
@@ -0,0 +1,21 @@
+from hashlib import sha256
+
+table = {}
+command = input("Enter command (login, register, exit): ")
+while command != 'exit':
+    if command == "register":
+        username = input("Enter new username : ")
+        password = input('Enter new password: ')
+        encryption_text = sha256(password.encode('utf-8')).hexdigest()
+        # print(encryption_text)
+        table[username] = encryption_text
+    elif command == "login":
+        username = input("Enter username : ")
+        password = input("Enter password: ")
+        check_hash = sha256(password.encode('utf-8')).hexdigest()
+        if table[username] == check_hash:
+            print("Welcome")
+        else:
+            print("Wrong password")
+
+    command = input("Enter command (login, register, exit): ")
